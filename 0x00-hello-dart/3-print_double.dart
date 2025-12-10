f0a90a12f4f03ca8bd336087ab0c01123d05941c
SDINAHET <stephane.dinahet@gmail.com>
2025-12-10 12:59:49 +0100

    task3

--- 0x00-hello-dart/3-print_double.dart
+++ 0x00-hello-dart/3-print_double.dart
@@ -1,4 +1,9 @@
+// void main() {
+//   var n = 3.14159265359;
+//   print("Double: ${n.toStringAsFixed(2)}");
+// }
+
 void main() {
-  var n = 3.14159265359;
-  print("Double: ${n.toStringAsFixed(2)}");
+  var number = 3.14159265359;
+  print("Double: ${number.toStringAsFixed(2)}");
 }
